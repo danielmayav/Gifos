9556cf24c3e8c88f8a57edca5f9aa0b72aa02aac
Daniel <danielmv92@gmail.com>
2021-03-30 23:07:01 -0500

    23:06 mar 30, Modo nocturno listo

--- app/js/script.js
+++ app/js/script.js
@@ -5,6 +5,9 @@ const hamburguerBtn = document.querySelector('#hamburguerBtn');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const navLinks = document.querySelector('#nav-links');
+const main = document.querySelector('.main');
+const body = document.body;
+
 
 hamburguerBtn.addEventListener('click', function(){
     console.log('click hamburguer');
@@ -22,8 +25,22 @@ hamburguerBtn.addEventListener('click', function(){
         overlay.classList.remove('overlay-animation');
         //Nav links
         navLinks.classList.remove('nav-links');
-        navLinks.classList.add('hide-for-mobile');
+        navLinks.classList.add('hide-for-mobile')
     }
+    
 });
 
 
+// MODO NOCTURNO
+
+function diurnoNocturno() {
+    var modo = document.getElementById("modnoct-btn");
+    if (modo.innerHTML === "MODO NOCTURNO") {
+      modo.innerHTML = "MODO DIURNO";
+      body.classList.add('dark');
+     
+    } else {
+      modo.innerHTML = "MODO NOCTURNO";
+      body.classList.remove('dark');
+     
+    }};
