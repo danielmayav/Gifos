be164ea1e51c51c62c46da18ad0ee43456c8c480
Daniel <danielmv92@gmail.com>
2021-05-10 09:56:20 -0500

    may 10 - 9:55, connected with API

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@ const apiKey = "qgKLO2sdzTr5Cniqn4m1BUGs6UiWP5bl";
 const searchGif = document.querySelector('#searchGif');
 const querySearch = 'cats';
 
-const searchPath = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${querySearch}&limit=10`;
+const searchPath = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${querySearch}&limit=12`;
 
 
 fetch(searchPath)
@@ -10,17 +10,22 @@ fetch(searchPath)
     return res.json()
 })
 .then(function(json){
-    const searchResult = document.querySelector('#results');
+    const searchResult = document.getElementById('results');
     let resultHTML = ''
 
     json.data.forEach(function(obj){
       console.log(obj.images.downsized.url)
-      const url = obj.images.downsized.url;
-      resultHTML += `<img src="${url}>"`;
+      let urlPath = obj.images.downsized.url;
+      resultHTML = document.createElement("img")
+      resultHTML.src = urlPath;
+      resultHTML.classList.add('gif')
+      searchResult.appendChild(resultHTML)
+      
 
    })
-
-   searchResult.innerHTML = resultHTML;
+   
+   
+   
 }) .catch (function(err){
     console.log(err.message)
 })
