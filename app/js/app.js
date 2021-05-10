1814e78986078291368153a50d5b2dbd81b83f53
Daniel <danielmv92@gmail.com>
2021-05-09 21:35:11 -0500

    may 9 21:334, fetch

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,28 @@
 const apiKey = "qgKLO2sdzTr5Cniqn4m1BUGs6UiWP5bl";
 const searchGif = document.querySelector('#searchGif');
+const querySearch = 'cats';
 
-const querySearch = searchGif.addEventListener('submit', function(e){
-    e.preventDefault();
-    e.targer.elements.searchBar.value = ''
-    
+const searchPath = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${querySearch}&limit=10`;
+
+
+fetch(searchPath)
+.then(function(res){
+    return res.json()
+})
+.then(function(json){
+    const searchResult = document.querySelector('#results');
+    let resultHTML = ''
+
+    json.data.forEach(function(obj){
+      console.log(obj.images.downsized.url)
+      const url = obj.images.downsized.url;
+      resultHTML += `<img src="${url}>"`;
+
+   })
+
+   searchResult.innerHTML = resultHTML;
+}) .catch (function(err){
+    console.log(err.message)
 })
 
-const searchPath = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${querySearch}&limit=36`;
 
-console.log(searchGif);
